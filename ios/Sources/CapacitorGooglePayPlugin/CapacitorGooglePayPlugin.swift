8f7bf185d074ccf3e0a0e3959a79828da5bdbb96
Florin Iacob <florin.iacob@gmail.com>
2024-12-20 19:16:55 +0200

    ios implementation

--- ios/Sources/CapacitorGooglePayPlugin/CapacitorGooglePayPlugin.swift
+++ ios/Sources/CapacitorGooglePayPlugin/CapacitorGooglePayPlugin.swift
@@ -10,7 +10,11 @@ public class CapacitorGooglePayPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "CapacitorGooglePayPlugin"
     public let jsName = "CapacitorGooglePay"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "init", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "isReadyToPay", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "loadPaymentData", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "completeAuthorization", returnType: CAPPluginReturnPromise)
     ]
     private let implementation = CapacitorGooglePay()
 
@@ -19,5 +23,23 @@ public class CapacitorGooglePayPlugin: CAPPlugin, CAPBridgedPlugin {
         call.resolve([
             "value": implementation.echo(value)
         ])
+    }
+
+    @objc func init(_ call: CAPPluginCall) {
+        call.resolve()
+    }
+
+    @objc func isReadyToPay(_ call: CAPPluginCall) {
+        call.resolve([
+            "result": false
+        ])
+    }
+
+    @objc func loadPaymentData(_ call: CAPPluginCall) {
+        call.unavailable();
+    }
+
+    @objc func completeAuthorization(_ call: CAPPluginCall) {
+        call.unavailable();
     }
 }
